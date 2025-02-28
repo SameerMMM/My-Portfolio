900513568c84aa86aa6b984d9f80f1a87ccd0bf2
SameerMMM <sameer20011028@gmail.com>
2025-02-28 17:08:47 +0530

    CV updated

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/Sameer CV (Web app).pdf";
+import pdf from "../../../public/Sameer CV (Web app).pdf"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -18,9 +18,8 @@ function ResumeNew() {
         <div>
             <Container fluid className="resume-section">
                 <Row className="resume">
-                    <Document file={pdf}>
-                        <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-                    </Document>
+                    <iframe src="../../../public/Sameer CV (Web app).pdf"
+                        width="100%" height="800px"></iframe>
                 </Row>
 
                 <Row style={{ justifyContent: "center", position: "relative" }}>
